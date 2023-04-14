b34632003c49012658e5ce815c55318531f22a3b
Majid Igale <majidigale18@gmail.com>
2023-04-14 06:36:12 -0500

    finished last portion, figured out how toinclude arrays in loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -40,9 +40,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-let star = ['North', 'Galax','Pot','Rigel', 'Yellow']
-for( i=0; i<star.length; i++) {
-  console.log('loop of', star[i]);
+
+for( i=0; i<stars.length; i++) {
+  console.log('loop of', stars[i]);
 }
 
 // 3. While loops  
@@ -58,6 +58,16 @@ while(i<stars.length){
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+i=0
+const max = 5
+while(i<max){
+  console.log('count up to:' , i);
+  i++; 
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let index = 10;
+while(index>=5 ){
+  console.log('count backwards:' , index);
+  index--;
+}
